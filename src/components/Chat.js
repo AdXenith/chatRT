65aa4e146a66242d6b6127dc950c3809c9364254
Riho-Martin <rihomartin.haavasalu@novater.com>
2023-01-05 20:31:02 +0200

    working chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { pb } from '../pocketbase';
 import AccountSide from './AccountSide';
+import Channels from './Channels';
 
 function Chat({ currentUser, chatSelected }) {
 
-    async function sendMessage(e) {
+    const [chats, setChats] = useState([]);
+    
+    useEffect(() => {
+        async function fetchChats(){
+            setChats(await pb.collection('messages').getList(1,50, {
+                sort: 'created',
+                filter: 'channel = "' + chatSelected.id + '"',
+                expand: 'user',
+            }), []);
+        }
+        fetchChats();
+    }, []);
 
+    async function getUser(userId) {
+        let user = await pb.collection('users').getOne(userId);
+        let username = user.username;
+        return username;
+    }
+
+    async function sendMessage(e) {
         e.preventDefault();
 
         try {
@@ -17,7 +36,7 @@ function Chat({ currentUser, chatSelected }) {
             const record = await pb.collection('messages').create(data);
           } catch (error) {
             console.error(error);
-          }
+        }
     }
 
     return ( 
@@ -25,132 +44,25 @@ function Chat({ currentUser, chatSelected }) {
             <AccountSide 
                 currentUser={currentUser}/>
             
-            <div className='container bg-black w-2/3 rounded-t-[3rem] absolute top-12 right-[8%] h-2/3 rounded-b-[2rem]'>
+            <div className='container bg-black w-2/3 rounded-t-[3rem] absolute top-12 right-[8%] rounded-b-[2rem]'>
                 <div className="flex flex-row bg-lightgray rounded-l-full rounded-r-full border-b border-b-black">
                         <img src="/image.png" className="w-16 flex-none" />
                         <p className="text-2xl my-auto pl-6 text-white font-semibold w-[79.5%]">{chatSelected.name}</p>
                 </div>
 
                 <div className='chat-container h-[42rem]'>
-                <div className="container bg-scroll pt-6 relative">
-                    <img src="/image.png" className="w-8 h-8 ml-4 absolute bottom-0" />
-                    <div className="max-w-[90%] text-white bg-lightgray rounded-lg p-2 ml-14 h-fit">
-                        <p>Jim Bomersville</p>
-                        <p className='overflow-auto'>Hello peopl eof te internet i am talking aaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-                    </div>
-                </div>
 
-                <div className="container flex flex-row bg-scroll pt-4">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
+                    { (chats.length == 0) ? null :
+                    
+                    Object.entries(chats)[4][1].map(chat => 
+                        <div className="container bg-scroll pt-6 relative">
+                            <img src="/image.png" className="w-8 h-8 ml-4 absolute bottom-0" />
+                            <div className="max-w-[75%] text-white bg-lightgray rounded-lg p-2 ml-14 h-fit">
+                                <p className='mb-1 font-semibold'>{chat.expand.user.username}</p>
+                                <p className='overflow-auto'>{chat.text}</p>
+                            </div>
+                        </div>) }
 
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>hello babes</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
-                <div className="container flex flex-row bg-scroll pb-10">
-                    <img src="/image.png" className="w-10 h-10 flex-none mt-6 ml-4" />
-                    <div className="max-w-[65%] text-white bg-lightgray rounded-lg p-2 ml-1">
-                        <p>Jim Bomersville</p>
-                        <p>Hello peopl eof te internet i am talking</p>
-                    </div>
-                </div>
                 </div>
 
                 <div className="flex flex-row relative">
