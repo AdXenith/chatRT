a654a49addc878f9771f5520417a5a12dad35d39
Riho-Martin <rihomartin.haavasalu@novater.com>
2023-01-05 21:32:06 +0200

    working realtime functionality

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,28 +1,36 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { pb } from '../pocketbase';
 import AccountSide from './AccountSide';
-import Channels from './Channels';
 
 function Chat({ currentUser, chatSelected }) {
 
     const [chats, setChats] = useState([]);
+
+    async function fetchChats(){
+        setChats(await pb.collection('messages').getList(1,50, {
+            sort: 'created',
+            filter: 'channel = "' + chatSelected.id + '"',
+            expand: 'user',
+        }), []);
+    }
+
+    function updateScroll(){
+        var element = document.getElementById("yourDivID");
+        element.scrollTop = element.scrollHeight;
+    }
     
     useEffect(() => {
-        async function fetchChats(){
-            setChats(await pb.collection('messages').getList(1,50, {
-                sort: 'created',
-                filter: 'channel = "' + chatSelected.id + '"',
-                expand: 'user',
-            }), []);
-        }
-        fetchChats();
-    }, []);
 
-    async function getUser(userId) {
-        let user = await pb.collection('users').getOne(userId);
-        let username = user.username;
-        return username;
-    }
+        if (chats.length == 0) { 
+            fetchChats();
+            
+        };
+
+        pb.collection('messages').subscribe('*', async function(e) {
+            console.log(e);
+            fetchChats();
+        })
+    });
 
     async function sendMessage(e) {
         e.preventDefault();
@@ -34,6 +42,8 @@ function Chat({ currentUser, chatSelected }) {
               channel: chatSelected.id,
             };
             const record = await pb.collection('messages').create(data);
+
+            document.getElementById('message').value = "";
           } catch (error) {
             console.error(error);
         }
@@ -50,29 +60,27 @@ function Chat({ currentUser, chatSelected }) {
                         <p className="text-2xl my-auto pl-6 text-white font-semibold w-[79.5%]">{chatSelected.name}</p>
                 </div>
 
-                <div className='chat-container h-[42rem]'>
+                <div className='chat-container h-[42rem] pb-16'>
 
                     { (chats.length == 0) ? null :
                     
                     Object.entries(chats)[4][1].map(chat => 
-                        <div className="container bg-scroll pt-6 relative">
+                        <div className="container pt-6 relative">
                             <img src="/image.png" className="w-8 h-8 ml-4 absolute bottom-0" />
                             <div className="max-w-[75%] text-white bg-lightgray rounded-lg p-2 ml-14 h-fit">
                                 <p className='mb-1 font-semibold'>{chat.expand.user.username}</p>
-                                <p className='overflow-auto'>{chat.text}</p>
+                                <p className='break-words'>{chat.text}</p>
                             </div>
                         </div>) }
 
                 </div>
 
-                <div className="flex flex-row relative">
+                <div className="flex flex-row relative rounded-full border-t border-black">
                     <img src="/image.png" className="w-14 flex-none absolute" />
                     <form onSubmit={sendMessage} className="flex flex-row h-14 w-full">
-                        <input type={'text'} id='message' className="pl-16 w-full bg-lightgray rounded-l-full" placeholder="Send a message..."></input>
+                        <input type={'text'} id='message' className="pl-16 pr-16 w-full text-white bg-lightgray rounded-full" placeholder="Send a message..." required></input>
 
-                        <input type={'submit'} id='send' value={"hello"} className="">
-
-                        </input>
+                        <input type={'image'} src="/send.png" alt='submit' id='send' className="w-14 absolute right-0 opacity-60 hover:opacity-100 hover:outline hover:outline-2 hover:outline-white rounded-full"></input>
                     </form>
                 </div>
             </div>
